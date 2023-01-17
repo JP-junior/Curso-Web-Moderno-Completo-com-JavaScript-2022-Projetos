c35f78105191ae21559dc572e7045790632cced6
Jorge Paduanelli <jorge.paduanelli.junior@gmail.com>
2023-01-17 20:56:35 -0300

    Aula 19 - Comentários de Código

--- Fundamentos/inicio/sentenca_1.js
+++ Fundamentos/inicio/sentenca_1.js
@@ -1,1 +1,5 @@
-
+/*Nessa aula foi explicado sobre a forma como as sentenças podem ser escritas, e que, apesar de recomendado, não é necessário colocar ';' no final da sentença*/
+console
+        .log("Bom dia!");
+console.log("Boa tarde!");
+console.log("Boa noite!");
---
+++ Fundamentos/inicio/sentenca_2.js
@@ -0,0 +1,4 @@
+/*Essa aula comentou sobre a ordem de execução das sentenças*/
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');
